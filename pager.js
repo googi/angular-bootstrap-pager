e017e58f3f940f67d2e53ffcca47ba64563f1d8b
zou <zou@ubuntusvr.(none)>
2014-04-11 23:18:48 +0800

    first init

--- pager.js
+++ pager.js
@@ -1,1 +1,12 @@
-var pg = {}
+var myModule = angular.module("myApp");
+
+myModule.directive("pg", function(injectables) {
+	directiveDefinitionObject = {
+			template: '<div></div>',
+			replace: true,
+			scope: true,
+			link: function(scope, iElement, iAttrs) {
+			}
+	};
+	return directiveDefinitionObject;
+});
